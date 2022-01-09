1befc777f5a08dc2342700a962fc1b75baa9f844
Bishnudev Khutia <91979889+bishnudev1@users.noreply.github.com>
2022-01-09 16:40:01 +0530

    js files

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    yourName: "                        Bishnudev Khutia",
+    Mail: "bishnudevkhutia@gmail.com",
+    twitterHandle: "@Excel28605690",
+    workat: "Computer Science & Engineering Student at",
+    workPlace: "CEMK",
+    twitterUsername: "Excel28605690",
+    githubUsername: "bishnudev1",
+    linkedinUsername: "bishnudev-khutia-a14942207/",
+    websiteUrl: "https://bishnudev.ml",
+    npxName: "bishnudev",
+};
